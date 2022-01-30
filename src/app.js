4a6ff54b7bf5194f8731b4102bc06e59b443b2e1
VAE94 <andreeavaduva1994@gmail.com>
2022-01-30 18:53:11 +0200

    -Modified the details page
    -Added functionalities to the cart, more to come

--- src/app.js
+++ src/app.js
@@ -8,12 +8,12 @@ window.addEventListener('load', async () => {
 	const cards = products
 		.map(
 			(product) =>
-				`<div class="card" style="width: 18rem;">
+				`<div class="card">
                  <div class="card-body">
-                  <img src="${product.image}"/>
+                  <img class="card-img" src="${product.image}" alt="Product Image"/>
                   <h5 class="card-title">${product.name}</h5>
                   <p class="card-text">${product.price}</p>
-                  <a href="details.html?product-id=${product.id}" class="btn btn-primary">Details</a>
+                  <a href="details.html?product-id=${product.id}" class="btn-details">Details</a>
                </div>
             </div>`
 		)
@@ -21,4 +21,3 @@ window.addEventListener('load', async () => {
 
 	productContainer.innerHTML = cards;
 });
-
--- src/details.js
+++ src/details.js
@@ -1,8 +1,46 @@
-export function showDetails(){
-    console.log('details');
-}
+window.addEventListener('load', async () => {
+	let searchParamString = window.location.search;
 
-export function showDetails2(){
-    console.log('details');
+	const searchParam = new URLSearchParams(searchParamString);
+	const productId = searchParam.get('product-id');
+
+	const productsURL = `https://61f2da932219930017f50933.mockapi.io/Products/${productId}`;
+	const result = await fetch(productsURL);
+	const product = await result.json();
+
+	const productCard = `
+            <div class="card-details">
+			<div class="card-body-details">
+            <img class="card-img" src="${product.image}" alt="Product Image"/>
+	  			<h5 class="card-title-details">${product.name}</h5>
+                <p class="card-text-details">${product.price}</p>
+	  			<p class="card-description">${product.description}</p>
+	  			<button data-product-id=${product.id} class="btn-cart">Add to cart</button>
+			</div>
+ 		</div>`;
+
+	document.querySelector('.product-details').innerHTML = productCard;
+});
+
+document.querySelector('.product-details').addEventListener('click', addToCart);
+async function addToCart(event) {
+	const addToCartBtn = event.target;
+	let productId = addToCartBtn.getAttribute('data-product-id');
+    
+    const productsURL = `https://61f2da932219930017f50933.mockapi.io/Products/${productId}`;
+	const result = await fetch(productsURL);
+	const product = await result.json();
+
+	let cart;
+	if (localStorage.getItem('cart') == null) {
+		cart = [product];
+	} else {
+		cart = JSON.parse(localStorage.getItem('cart'));
+		cart.push(product);
+	}
+
+	console.log(cart);
+
+	localStorage.setItem('cart', JSON.stringify(cart));
 }
 
