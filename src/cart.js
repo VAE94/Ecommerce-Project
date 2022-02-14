d99f1c5be16a0e2d6f8a4ef26c62f1217146bfa0
VAE94 <andreeavaduva1994@gmail.com>
2022-02-14 21:22:04 +0200

    -continued with cart
    -more style in cart and details

--- src/cart.js
+++ src/cart.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', () => {
-	const products = JSON.parse(localStorage.getItem('cart'));
+	const cart = JSON.parse(localStorage.getItem('cart'));
 
 	let total = 0;
-	if (products) {
-		products.forEach((product) => {
-			total = total + Number(product.price);
+	if (cart) {
+		cart.forEach((product) => {
+			total = total + Number(product.price) * product.noOfProducts;
 			console.log(product.price);
 		});
 
-		const productCards = products
+		const productCards = cart
 			.map(
 				(product) =>
-			`<div class="card w-75 mt-4">
+			 `<div class="card w-75 mt-4">
               <div class="card-body mx-auto">
 			  <img class="card-img-top img-fluid" style="width:250px;" src="${product.image}" alt="Product Image"/>
                <h6 class="card-title text-center mt-2">${product.name}</h6>
@@ -20,16 +20,18 @@ window.addEventListener('load', () => {
 			  <button  data-product-id=${product.id} class="remove-products cart-btn"> - </button>
 		    <span class="cart-products"> ${product.noOfProducts}</span>
 			<button  data-product-id=${product.id} class="add-products cart-btn"> + </button>
-			
 			</p>
         </div>
+		<button  data-product-id=${product.id} class="empty-cart"> EMPTY CART </button>
         </div>`
 			)
 			.join('');
 
-		let totalPriceCard = `<div class="mt-2 border-total" style="width:100px;">TOTAL: ${total} €</div>`;
+		let totalPriceCard = `<div class="total-price">TOTAL: ${total} €</div>`;
 		document.querySelector('.cart-container').innerHTML =
-			productCards + totalPriceCard;
+			productCards;
+
+			document.querySelector('.total-price-container').innerHTML = totalPriceCard;
 	}
 });
 
@@ -39,20 +41,38 @@ cartContainer.addEventListener('click', handleCartEvents);
 function handleCartEvents(event) {
 	const targetButton = event.target;
 	let cart = JSON.parse(localStorage.getItem('cart'));
+	const productInCart = cart.find(
+		(productFromCart) =>
+			productFromCart.id == targetButton.getAttribute('data-product-id')
+	);
+	let quantityParagraph = targetButton.parentNode;
 
 	if (targetButton.classList.contains('add-products')) {
-		const productInCart = cart.find(
-			(productFromCart) =>
-				productFromCart.id == targetButton.getAttribute('data-product-id')
-		);
-		console.log(productInCart, 'product in cart');
 		productInCart.noOfProducts++;
 	} else if (targetButton.classList.contains('remove-products')) {
-		console.log('butonul de minus');
-		const productInCart = cart.find(
-			(productFromCart) =>
-				productFromCart.id == targetButton.getAttribute('data-product-id')
+		if (productInCart.noOfProducts > 0) productInCart.noOfProducts--;
+	} else if (targetButton.classList.contains('empty-cart')) {
+		productInCart.noOfProducts = 0;
+		cart = cart.filter((product) => product.id != productInCart.id);
+
+		console.log(
+			(cart = cart.filter((product) => product.id != productInCart.id))
 		);
-		productInCart.noOfProducts--;
+		targetButton.parentNode.remove();
 	}
+
+	localStorage.setItem('cart', JSON.stringify(cart));
+	if (productInCart){
+		quantityParagraph.querySelector('.cart-products').innerHTML =
+			productInCart.noOfProducts;
+
+			let total = 0;
+			cart.forEach((product) => {
+				total = total + Number(product.price) * product.noOfProducts;
+				console.log(product.price);
+			});
+	
+			let totalPriceCard = `<div class="total-price">TOTAL: ${total} €</div>`;
+			document.querySelector('.total-price-container').innerHTML = totalPriceCard;
+		}
 }
